9d3ae9fb61057ec12232cf7747aedf7c83150802
Boris Bossink <b.bossink@student.fontys.nl>
2022-12-21 15:48:32 +0100

    Created new E2E test for generation

--- cypress/e2e/character-creation.cy.ts
+++ cypress/e2e/character-creation.cy.ts
@@ -2,10 +2,8 @@ import { e2eUtils } from "./e2e-utils";
 
 describe('Create a character, then view it and delete it', () => {
   before(() => cy.login('http://localhost:4200/character', 'boris', 'wachtwoord123'));
-  beforeEach(() => cy.restoreLocalStorage());
-  afterEach(() => cy.saveLocalStorage());
+
   it('visit', () => {
-    cy.visit("http://localhost:4200/character")
     cy.contains('My Characters').click();
     cy.url().should('eq', 'http://localhost:4200/character');
     cy.contains('Create a new character').click();
@@ -17,9 +15,10 @@ describe('Create a character, then view it and delete it', () => {
     cy.restoreLocalStorage();
     const textFields = e2eUtils.inputOfType('text');
     const numFields = e2eUtils.inputOfType('number');
+    const dropdowns = cy.get('select');
     e2eUtils.inputWithName(textFields, 'name').type('My Awesome Character');
-    e2eUtils.inputWithName(textFields, 'race').type('Human');
-    e2eUtils.inputWithName(textFields, 'class').type('Wizard');
+    e2eUtils.inputWithName(dropdowns, 'race').select(3);
+    e2eUtils.inputWithName(dropdowns, 'class').select(3);
     e2eUtils.inputWithName(numFields, 'strength').type('1');
     e2eUtils.inputWithName(numFields, 'dexterity').type('2');
     e2eUtils.inputWithName(numFields, 'constitution').type('3');
@@ -27,11 +26,13 @@ describe('Create a character, then view it and delete it', () => {
     e2eUtils.inputWithName(numFields, 'wisdom').type('5');
     e2eUtils.inputWithName(numFields, 'charisma').type('6');
     e2eUtils.inputOfType('submit').click();
-  });
-
-  it('view', () => {
     cy.url().should('eq', 'http://localhost:4200/character');
     cy.contains('My Awesome Character').click();
+    cy.saveLocalStorage();
+  });
+
+  it('delete', () => {
+    cy.restoreLocalStorage();
     cy.contains('Delete').click();
     cy.url().should('eq', 'http://localhost:4200/character');
   })
---
+++ cypress/e2e/stat-generation.cy.ts
@@ -0,0 +1,40 @@
+import { waitForAsync } from "@angular/core/testing";
+import { result } from "cypress/types/lodash";
+import { e2eUtils } from "./e2e-utils";
+
+describe('empty spec', () => {
+  it('generate character stats', () => {
+    cy.intercept('POST', 'https://localhost:5001/api/generate').as('generateRequest');
+    cy.visit('http://localhost:4200/character/create');
+    cy.get('button').click();
+    cy.get('mat-dialog-container').should('exist');
+    let inp = e2eUtils.inputOfType('number');
+    e2eUtils.inputWithName(inp, 'd20').type('1');
+    e2eUtils.inputWithName(inp, 'd12').type('2');
+    e2eUtils.inputWithName(inp, 'd10').type('3');
+    e2eUtils.inputWithName(inp, 'd8').type('4');
+    e2eUtils.inputWithName(inp, 'd6').type('5');
+    e2eUtils.inputWithName(inp, 'd4').type('6');
+    cy.get('button').get("[id='gen']").click();
+
+    cy.wait('@generateRequest').should(r => {
+      expect(r.response).to.have.property('statusCode', 200);
+    });
+
+    let results: string[] = [];
+    cy.get('data').each(((el) => {
+      cy.wrap(el).invoke('text').then(t => results.push(t));
+    })).then(() => {
+      e2eUtils.inputOfType('submit').get("[value='Use these stats']").click();
+      cy.get('mat-dialog-container').should('not.exist');
+      const nums = e2eUtils.inputOfType('number');
+      e2eUtils.inputWithName(nums, 'strength').should('have.value', results[0]);
+      e2eUtils.inputWithName(nums, 'dexterity').should('have.value', results[1]);
+      e2eUtils.inputWithName(nums, 'constitution').should('have.value', results[2]);
+      e2eUtils.inputWithName(nums, 'intelligence').should('have.value', results[3]);
+      e2eUtils.inputWithName(nums, 'wisdom').should('have.value', results[4]);
+      e2eUtils.inputWithName(nums, 'charisma').should('have.value', results[5]);
+    });
+    
+  })
+})
